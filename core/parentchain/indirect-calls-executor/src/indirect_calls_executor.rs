cb4005d2d4de5b4c0e9976f1fe8657a7a347e1f2
Gaudenz Kessler <gaudenz.kessler@scs.ch>
2022-02-15 14:35:05 +0100

    fmt and rename

--- core/parentchain/indirect-calls-executor/src/indirect_calls_executor.rs
+++ core/parentchain/indirect-calls-executor/src/indirect_calls_executor.rs
@@ -20,7 +20,9 @@
 use crate::error::Result;
 use codec::{Decode, Encode};
 use ita_stf::{AccountId, TrustedCallSigned};
-use itp_settings::node::{ACK_GAME, CALL_WORKER, GAME_REGISTRY_MODULE, SHIELD_FUNDS, TEEREX_MODULE};
+use itp_settings::node::{
+	ACK_GAME, CALL_WORKER, GAME_REGISTRY_MODULE, SHIELD_FUNDS, TEEREX_MODULE,
+};
 use itp_sgx_crypto::ShieldingCrypto;
 use itp_stf_executor::traits::{StatePostProcessing, StfExecuteShieldFunds, StfExecuteTrustedCall};
 use itp_types::{AckGameFn, CallWorkerFn, OpaqueCall, ShardIdentifier, ShieldFundsFn, H256};
@@ -109,7 +111,7 @@ where
 	{
 		debug!("Scanning block {:?} for relevant xt", block.header().number());
 		let mut opaque_calls = Vec::<OpaqueCall>::new();
-		let mut executed_shielding_calls = Vec::<H256>::new();
+		let mut executed_extrinsics = Vec::<H256>::new();
 		for xt_opaque in block.extrinsics().iter() {
 			// Found ShieldFunds extrinsic in block.
 			if let Ok(xt) =
@@ -120,25 +122,24 @@ where
 						error!("Error performing shield funds. Error: {:?}", e);
 					} else {
 						// Cache successfully executed shielding call.
-						executed_shielding_calls.push(hash_of(xt))
+						executed_extrinsics.push(hash_of(xt))
 					}
 				}
 			};
 
 			// Found Ack_Game extrinsic in block.
 			if let Ok(xt) =
-			UncheckedExtrinsicV4::<AckGameFn>::decode(&mut xt_opaque.encode().as_slice())
+				UncheckedExtrinsicV4::<AckGameFn>::decode(&mut xt_opaque.encode().as_slice())
 			{
 				if xt.function.0 == [GAME_REGISTRY_MODULE, ACK_GAME] {
 					if let Err(e) = self.handle_ack_game_xt(&xt) {
 						error!("Error performing acknowledge game. Error: {:?}", e);
 					} else {
 						// Cache successfully executed shielding call.
-						executed_shielding_calls.push(hash_of(xt))
+						executed_extrinsics.push(hash_of(xt))
 					}
 				}
 			};
-
 
 			// Found CallWorker extrinsic in block.
 			if let Ok(xt) =
@@ -161,7 +162,7 @@ where
 				}
 			}
 		}
-		Ok((opaque_calls, executed_shielding_calls))
+		Ok((opaque_calls, executed_extrinsics))
 	}
 }
 
