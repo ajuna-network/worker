bc69ec20c667a7fb2287ca25109aa93e4719cf7f
gaudenzkessler <92718752+gaudenzkessler@users.noreply.github.com>
2022-02-15 09:05:16 +0100

    Create and send signed ack_game extrinsic (#7)
    * send extrinsic ack_game.
    
    * add Shard Identifier to Extrinsic
    
    Cherry-pick: ca24498
    
    Co-authored-by: Gaudenz Kessler <gaudenz.kessler@scs.ch>

--- core-primitives/registry-storage/src/lib.rs
+++ core-primitives/registry-storage/src/lib.rs
@@ -1,18 +1,17 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-use codec::{Decode, Encode};
-use sp_std::prelude::Vec;
-
 use itp_storage::{storage_map_key, StorageHasher};
+use pallet_ajuna_gameregistry::game::GameEngine;
+use sp_std::prelude::Vec;
 
 pub const REGISTRY: &str = "GameRegistry";
 
 pub struct RegistryStorage;
 
 // Separate the prefix from the rest because in our case we changed the storage prefix due to
-// the rebranding. With the below implementation of the `TeerexStorageKeys`, we could simply
+// the rebranding. With the below implementation of the `RegistryStorageKeys`, we could simply
 // define another struct `OtherStorage`, implement `StoragePrefix` for it, and get the
-// `TeerexStorageKeys` implementation for free.
+// `RegistryStorageKeys` implementation for free.
 pub trait StoragePrefix {
 	fn prefix() -> &'static str;
 }
@@ -27,15 +26,9 @@ pub trait RegistryStorageKeys {
 	fn queue_game() -> Vec<u8>;
 }
 
-#[derive(Encode)]
-pub struct GameEngine {
-	id: u8,
-	version: u8,
-}
-
 impl<S: StoragePrefix> RegistryStorageKeys for S {
 	fn queue_game() -> Vec<u8> {
-		let game_engine = GameEngine { id: 1u8, version: 1u8 };
+		let game_engine = GameEngine::new(1u8, 1u8);
 		storage_map_key(Self::prefix(), "GameQueues", &game_engine, &StorageHasher::Identity)
 	}
 }
