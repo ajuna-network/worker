bb4be87d0df3c360281a702e950e95d8b5953d23
Andy Bell <andyjsbell@gmail.com>
2022-06-10 16:49:12 +0200

    fmt

--- core-primitives/stf-executor/src/executor.rs
+++ core-primitives/stf-executor/src/executor.rs
@@ -230,7 +230,7 @@ where
 				let (state_lock, mut state) = self.state_handler.load_for_mutation(shard)?;
 				let root = Stf::get_root(&mut state);
 				let nonce = Stf::account_nonce(&mut state, &root);
-				
+
 				if let RunnerState::Accepted(mut runner_state) = runner {
 					if let Ok(game) = Game::<AccountId>::decode(&mut runner_state) {
 						if game.players.len() == 2 {
