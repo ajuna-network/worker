0d0b9ce19444c285b03822a87f025fc287c19df7
haerdib <bhaerdi@devsgx02.scs-ad.scs.ch>
2022-11-21 16:13:03 +0100

    replace sub0.ajuna with ajun02

--- enclave-runtime/src/initialization.rs
+++ enclave-runtime/src/initialization.rs
@@ -383,7 +383,7 @@ pub(crate) fn init_direct_invocation_server(server_addr: String) -> EnclaveResul
 	let rpc_handler = GLOBAL_RPC_WS_HANDLER_COMPONENT.get()?;
 	let signing = Ed25519Seal::unseal_from_static_file()?;
 
-	let cert = ed25519_self_signed_certificate(signing, "sub0.ajuna.network")
+	let cert = ed25519_self_signed_certificate(signing, "ajuna-02.cluster.securitee.tech")
 		.map_err(|e| Error::Other(e.into()))?;
 
 	//write certificate and private key pem file
